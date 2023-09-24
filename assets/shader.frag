b79862c31a260a82270ea9eeeba03f4918406d1d
Yoshihiro Tanaka <contact@cordea.jp>
2023-09-24 14:10:39 +0900

    display the icon

--- assets/shader.frag
+++ assets/shader.frag
@@ -1,4 +1,12 @@
 #include <flutter/runtime_effect.glsl>
 
+uniform vec2 uSize;
+uniform sampler2D uTexture;
+
+out vec4 fragColor;
+
 void main() {
+    vec2 uv = FlutterFragCoord().xy / uSize;
+    vec4 tx = texture(uTexture, uv);
+    fragColor = tx;
 }
--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,18 @@
 import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final fragmentShaderProvider =
     FutureProvider((ref) => ui.FragmentProgram.fromAsset('assets/shader.frag'));
+
+final iconProvider = FutureProvider((ref) async {
+  final data = await ui.ImmutableBuffer.fromAsset('assets/icon.png');
+  final codec = await ui.instantiateImageCodecFromBuffer(data);
+  final info = await codec.getNextFrame();
+  return info.image;
+});
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -22,10 +30,14 @@ class _Body extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final shader = ref.watch(fragmentShaderProvider);
+    final icon = ref.watch(iconProvider);
     final Widget child;
-    if (shader.hasValue) {
+    if (shader.hasValue && icon.hasValue) {
       child = CustomPaint(
-        painter: _Painter(shader.requireValue.fragmentShader()),
+        painter: _Painter(
+          shader.requireValue.fragmentShader(),
+          icon.requireValue,
+        ),
         child: const AspectRatio(aspectRatio: 1),
       );
     } else {
@@ -39,15 +51,19 @@ class _Body extends HookConsumerWidget {
 }
 
 class _Painter extends CustomPainter {
-  _Painter(this.shader);
+  _Painter(this.shader, this.image);
 
   final ui.FragmentShader shader;
+  final ui.Image image;
 
   @override
   void paint(ui.Canvas canvas, ui.Size size) {
     if (size.isEmpty) {
       return;
     }
+    shader.setFloat(0, size.width);
+    shader.setFloat(1, size.height);
+    shader.setImageSampler(0, image);
     canvas.drawRect(
       Rect.fromLTWH(0, 0, size.width, size.height),
       Paint()..shader = shader,

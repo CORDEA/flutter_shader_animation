05f2bcb996d20f47b7929b634721b7beef201814
Yoshihiro Tanaka <contact@cordea.jp>
2023-09-24 14:33:00 +0900

    change the background

--- lib/home.dart
+++ lib/home.dart
@@ -21,6 +21,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(title: const Text('Home')),
       body: _Body(),
     );
